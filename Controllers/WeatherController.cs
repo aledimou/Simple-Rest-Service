ff1f75c4bf53d0ce98fd7347c533b69e474a289d
AlexD <71217689+aledimou@users.noreply.github.com>
2020-10-10 00:02:28 +0300

    Add files via upload

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,58 @@
+﻿using RestDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RestDemo.Controllers
+{
+    public class WeatherController : ApiController
+    {
+        // GET: api/Weather
+        public IEnumerable<WeatherInfo> Get()
+        {
+                 
+            try
+            {
+                var WeatherInfoList = new List<WeatherInfo>(); 
+                string[] City = new string[] { "London", "Paris", "Athens", "New York", "Berlin", "Vienna", "Moscow", "Tel-Aviv", "Zagreb", "Warsaw" };
+
+                for (int i = 0; i < City.Length; i++)
+                {
+                    var weatherInfo = new WeatherInfo
+                    {
+
+                        Location = City[i],
+                        Degree = 20 + 17 / 2 * i,
+                        DateTime = DateTime.Now.ToUniversalTime()
+
+                    };
+                    WeatherInfoList.Add(weatherInfo);
+                }
+                return WeatherInfoList;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            
+
+        }
+        // GET: api/Weather/5
+        public WeatherInfo Get(int id)
+        {
+            return new WeatherInfo
+            {
+                Location = ("Location " + id),
+                Degree = 20 + 17 / 2 * id,
+                DateTime = DateTime.Now.ToUniversalTime()
+            };
+
+        }
+    }
+}
---
+++ Models/WeatherInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace RestDemo.Models
+{
+    [DataContract]
+    public class WeatherInfo
+    {
+        [DataMember(Name = "Location")]
+        public string Location { get; set; }
+        [DataMember(Name = "degree")]
+        public double Degree { get; set; }
+        [DataMember(Name = "Datetime")]
+        public DateTime DateTime { get; set; }
+    }
+}
